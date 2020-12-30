d7f7ad232af4fa1b6393070cdbab6d605b3287bf
Na-naO <la.dolce.vita.nana@gmail.com>
2020-12-30 16:21:57 +0900

    Add info of DB

--- thanks.php
+++ thanks.php
@@ -6,3 +6,12 @@
   $situation = htmlspecialchars($_POST['situation']);
   $title = htmlspecialchars($_POST['title']);
   $content = htmlspecialchars($_POST['content']);
+
+
+  $dns = 'mysql:dbname=Omiyage_reminder;host=localhost';
+  $user = 'root';
+  $password ='';
+  $dbh = new PDO($dsn, $user, $password);
+  $dbh->query('SET NAMES utf8');
+
+
